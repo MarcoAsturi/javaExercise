393ef4e62449687afd1784eee30e8d4fe0526182
Asturi <marco.asturi@accenture.com>
2023-01-13 11:35:16 +0100

    added w7e1 exercise

--- W6E4RecursiveSquares.java
+++ W6E4RecursiveSquares.java
@@ -1,4 +1,6 @@
 public class W6E4RecursiveSquares {
+
+    //method for drawing a square centered in (x, y) of tuhe given side length
     public static void drawSquare(double x, double y, double length) {
         double x0 = x - length / 2;
         double x1 = x + length / 2;
@@ -9,7 +11,9 @@ public class W6E4RecursiveSquares {
         StdDraw.line(x1, y1, x0, y1);
         StdDraw.line(x0, y1, x0, y0);
     }
-
+    
+    // Draws a recursive square pattern of order n, centered on (x, y)
+    // of the given side length.
     public static void draw(int n, double x, double y, double length) {
         if (n == 0) {
             return;
@@ -24,7 +28,9 @@ public class W6E4RecursiveSquares {
         draw(n - 1, x1, y0, length / 2);
         draw(n - 1, x1, y1, length / 2);
     }
-
+    
+    // Takes an integer command-line argument n and draws a recursive
+    // square pattern of order n, centered on (0.5, 0.5) with side length 0.5.
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         draw(n, 0.5, 0.5, 0.5);
--- W7E1Inversoins.java
+++ W7E1Inversoins.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class W7E1Inversoins {
 
     // return the number of inversion in the permutation a[]
+    // given an array of integers, a pair of elements a[i] and a[j]
+    // are inverted if i < j and a[i] > a[j]
     public static long count(int[] a) {
         long count = 0;
         for (int i = 0; i < a.length; i++) {
@@ -13,13 +17,35 @@ public class W7E1Inversoins {
         return count;
     }
 
-    // public static int[] generate(int n, long k) {
+    // Return a permutation of length n with exactly k inversions.
+    public static int[] generate(int n, long k) {
 
-    // }
+        // creating array of length n
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
 
+        // permutation cycle (CAN'T WORK IF k >= n)
+        for (int i = 1; i <= k; i++) {
+            int temp = arr[n - i];
+            arr[n - i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+        return arr;
+    }
+
+    // Takes an integer n and a long k as command-line arguments,
+    // and prints a permutation of length n with exactly k inversions.
     public static void main(String[] args) {
-        //test count method
-        int[] a = {7, 1, 2, 4, 3, 6, 5};
-        System.out.println(count(a));
+        // test count method
+        // int[] a = {7, 1, 2, 4, 3, 6, 5};
+        // System.out.println(count(a));
+
+        // test generate method
+        int n = Integer.parseInt(args[0]);
+        int k = Integer.parseInt(args[1]);
+        System.out.println(Arrays.toString(generate(n, k)));
+        System.out.println(count(generate(n, k)));
     }
 }
